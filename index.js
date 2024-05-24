e070eef28b02c2d2f3271ca50762696ae19605ee
Maria Jose Jaramillo A <mariaj.jaramilloa@autonoma.edu.co>
2024-05-23 20:32:48 -0500

    creation of user crud

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const userRoutes = require('./src/routes/UserRoutes');
 
-async function main(){
-    console.log('Creating a new user...');
-}
+const app = express();
+app.use(bodyParser.json());
 
-main()
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+//RUTAS
+app.use('/users', userRoutes); //Mount the user routes
+app.use('/uploads', express.static('uploads'));
+require('dotenv').config();
+
+//conexion con BD
+const CONNECTION_PORT = process.env.PORT || 3002;
+app.listen(CONNECTION_PORT, () => {
+    console.log(`Server running on port ${CONNECTION_PORT}`);
+});
